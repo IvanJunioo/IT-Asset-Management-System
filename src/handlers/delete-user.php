b8383b8d1d85272fe2bdd2a0745d2fd4b28a834c
Levie Dianito <lmdianito@up.edu.ph>
2026-01-24 15:12:03 +0800

    add batch deactivate

--- src/handlers/delete-user.php
+++ src/handlers/delete-user.php
@@ -3,15 +3,29 @@
 require_once '../utilities/request-guard.php';
 require_once '../../config/config.php';
 require_once '../repos/user.php';
+require_once '../manager/assign.php';
+require_once '../repos/assignment.php';
 require_once '../manager/logger.php';
 
 $empID = $_POST['search'] ?? "";
 
-$repo = new UserRepo($pdo);
+$assignRepo = new AssignmentRepo($pdo);
+$userRepo = new UserRepo($pdo);
+$manag = new AssignmentManager(
+  new AssetRepo($pdo),
+  $assignRepo,
+  $userRepo,
+);
 
-$user = $repo->identify($empID);
+$user = $userRepo->identify($empID);
+$assets = $assignRepo->getAssignedAssets($user);
+$retDate = new DateTimeImmutable('now');
+foreach ($assets as $asset){
+  $manag->returnAsset($asset->propNum, $retDate, "");
+}
+
 $user->isActive = false;
-$repo->update($user);
+$userRepo->update($user);
 
 systemLog(
   "deactivated user $empID",
