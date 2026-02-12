f11fb90d910ebb415affe04e6cb3bdc215930767
Levie Dianito <dianitolevie53@gmail.com>
2026-02-12 09:03:51 +0800

    fix report generation

--- src/handlers/export-faculty-asset.php
+++ src/handlers/export-faculty-asset.php
@@ -1,4 +1,3 @@
-
 <?php
 require __DIR__ . '/../../vendor/autoload.php'; 
 require_once __DIR__ . '/../../config/config.php';
@@ -6,22 +5,28 @@ require_once __DIR__ . '/../repos/user.php';
 require_once __DIR__ . '/../repos/asset.php';
 require_once __DIR__ . '/../repos/assignment.php';
 
-ob_start();
 use Dompdf\Dompdf;
 
 try {
-    $usersID = explode(",",$_GET['users'] ?? "");
+    $usersParam = $_GET['users'] ?? null;
+    $usersID = [];
+    if (!empty($usersParam)) {
+        $usersID = array_filter(explode(",", $usersParam));
+    }
     $userRepo = new UserRepo($pdo);
     $assignRepo = new AssignmentRepo($pdo);
 
     $data = [];
     foreach ($usersID as $id) {
         $user = $userRepo->identify($id);
+        if (!$user) continue;
+
         $assets = $assignRepo->getAssignedAssets($user);
 
         $assetDates = [];
         foreach ($assets as $asset) {
-            $assetDates[$asset->propNum] = $assignRepo->getAssignmentDate($asset);
+            $assetDates[$asset->propNum] =
+                $assignRepo->getAssignmentDate($asset);
         }
 
         $data[] = [
@@ -33,23 +38,31 @@ try {
 
     $cssPath = __DIR__ . '/../../public/css/asset-pdf.css';
     $css = file_get_contents($cssPath);
+
+    ob_start();
     include '../template/faculty-assigned-assets.php';
     $html = ob_get_clean();
+
     $html = "<style>{$css}</style>" . $html;
 
     $dompdf = new Dompdf();
     $dompdf->loadHtml($html);
     $dompdf->setPaper('A4', 'portrait');
     $dompdf->render();
-    if (ob_get_length()) ob_end_clean();
 
-    $dompdf->stream("Faculty_assigned_assets.pdf", ["Attachment" => true]);
+    while (ob_get_level()) {
+        ob_end_clean();
+    }
+
+    $dompdf->stream("Faculty_assigned_assets.pdf", [
+        "Attachment" => true
+    ]);
     exit;
 
 } catch (Exception $e) {
-    if (ob_get_length()) ob_end_clean();
+    while (ob_get_level()) {
+        ob_end_clean();
+    }
     echo json_encode(["error" => $e->getMessage()]);
     exit;
 }
-
-
