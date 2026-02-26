62bfb54a42e138c35d984a3d00f7b158ec2d7417
Levie Dianito <dianitolevie53@gmail.com>
2026-02-26 18:16:50 +0800

    Add validation for duplicate email in form submission

--- public/script/add-user.js
+++ public/script/add-user.js
@@ -7,4 +7,42 @@ document.addEventListener("DOMContentLoaded", () => {
   resetBtn?.addEventListener("click", (_) => {
     form.reset();
   })
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("e");
+
+    let valid = true;
+    let dataEmail = await checkIfExists(email);
+
+    if (dataEmail) {
+      valid = false;
+      alert(`A user with this credentials already exists`);
+    }
+
+    if (valid) {
+      form.submit();
+    }
+  })
 });
+
+
+async function checkIfExists(input) {
+  const url = new URL(`${window.location.origin}/public/api/index.php`);
+  url.search = new URLSearchParams({
+    resource: "users",
+    action: "search",
+    search: input.value,
+  });
+
+  try {
+    const resp = await fetch(url);
+    if (!resp.ok) throw new Error(`HTTP error! status: ${resp.status}`);
+    const data = await resp.json();
+    if (data && data.length > 0) return data[0];
+  } catch (err) {
+    console.error("Error fetching users: ", err);
+  }
+  
+  return null;
+}
