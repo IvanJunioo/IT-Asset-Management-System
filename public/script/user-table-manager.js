ec56870dd12962a13c27c204820739ad03414bc6
Hedelito III Dollison <hmdollison@up.edu.ph>
2026-02-26 23:55:50 +0800

    rebase conflict

--- public/script/user-table-manager.js
+++ public/script/user-table-manager.js
@@ -74,11 +74,86 @@ document.addEventListener("click", (e) => {
   document.querySelectorAll(".action-menu").forEach(menu => {
     menu.style.display = "none";
   });
+
+
+  // Reports
+  if (e.target.closest("#report")) {
+    if (selectedRows.size <= 0) {
+      alert('Select a user first to get their assets');
+      return;
+    }
+
+    if (selectedRows.size == 1) {
+      const [tr] = selectedRows;
+      window.open(
+        `${window.location.origin}/public/api/index.php?resource=export&action=user-assets&user=` + encodeURIComponent(tr.dataset.empid),
+        "_blank"
+      );
+      return;
+    }
+
+    addReportModal();
+    return;
+  }
+
+  if (e.target.closest("#closeModal")) {
+    e.target.closest("#reportModal").remove();
+    return;
+  }
+
+  if (e.target.closest("#reportModal")) {
+    const target = e.target.closest(".report-option");
+    if (!target) return;
+
+    let users = [];
+    for (const tr of selectedRows) {
+      users.push(tr.dataset.empid);
+    }
+
+    switch (target.dataset.type) {
+      case "single":
+        
+        window.open(
+          `${window.location.origin}/public/api/index.php?resource=export&action=faculty-assets&users=` + encodeURIComponent(users),
+          "_blank"
+        );
+        break;
+      case "multiple":
+        window.open(
+          `${window.location.origin}/public/api/index.php?resource=export&action=faculty-assets-multiple&users=` + encodeURIComponent(users),
+          "_blank"
+        );
+        break;
+    }
+
+    e.target.closest("#reportModal").remove();
+    return;
+  }
+
 });
 
-tableFuncs.addEventListener("click", (e) => {
+tableFuncs.addEventListener("click", async (e) => {
+  if (e.target.closest("#multi-select")) {
+    const multiSelectBtn = e.target.closest("#multi-select");
+    
+    multiSelectBtn.classList.toggle('active');
+    
+    setInMulSel(!inMultiSelect);
+  }
+
   if (e.target.closest(".table-fn")) {
     const btn = e.target.closest(".table-fn");
+
+    if (btn.value === "deactivate") {
+      const hasAssignments = await checkAssignment();
+      
+      if (hasAssignments) {
+        const proceed = confirm(`User ${hasAssignments} currently have assets assigned to them. Do you wish to proceed?`);
+
+        if (!proceed) return;
+      }
+    }
+
     for (const tr of selectedRows) modifyUser(tr.dataset.empid, btn.value);
   }
 });
@@ -149,8 +224,79 @@ function updateTableFuncs() {
   ) ? "flex" : "none";
 
   const deactBtn = tableFuncs.querySelector('button[name="deactivate"]');
-  deactBtn.style.display = inMultiSelect && selectedRows.size > 0 && [...selectedRows].every(tr =>
-    tableData.get(Number(tr.dataset.empid))?.ActiveStatus === "Active" &&
-    tableData.get(Number(tr.dataset.empid))?.EmpID !== JSON.parse(sessionStorage.getItem("user-info")).EmpID
-  ) ? "flex" : "none";
+
+  deactBtn.style.display = [...selectedRows].every(tr => tableData.get(Number(tr.dataset.empid)).ActiveStatus === "Active" && tableData.get(Number(tr.dataset.empid)).EmpID !== JSON.parse(sessionStorage.getItem("user-info")).EmpID)? "flex" : "none";
+}
+
+function updateSelectedRows() {
+  var toAdd = new Set();
+  var toDel = new Set();
+
+  for (const tr1 of userTableBody.querySelectorAll("tr")) {
+    if (tr1.dataset.activeStatus === "Inactive" ||
+      session.user_id === tr1.dataset.empID
+    ) {continue;}
+
+    for (const tr2 of selectedRows) {
+      if (tr2.dataset.empID === tr1.dataset.empID) {
+        toDel.add(tr2);
+        toAdd.add(tr1);
+      }
+    }
+  }
+
+  for (const tr of toDel) {
+    selectedRows.delete(tr)
+  }
+
+  for (const tr of toAdd) {
+    selectedRows.add(tr)
+  }
+}
+
+function addReportModal() {
+  const modalDiv = document.createElement("div");
+  modalDiv.id = "reportModal";
+  modalDiv.className = "modal";
+  modalDiv.innerHTML = `
+    <div class="modal-content">
+      <div class="modal-header">Choose Export Type
+      </div>
+      <div class="modal-body">
+        <button class="report-option" data-type = "single"> 
+          Download selected assets (Single PDF)
+        </button>
+        <button class="report-option" data-type="multiple"> 
+          Download selected assets separately (Multiple PDFs)
+        </button>
+      </div>
+      <button id="closeModal" class="btn-cancel">Cancel</button>
+    </div>
+  `;
+  modalDiv.style.display = 'block';
+  document.body.appendChild(modalDiv);
+}
+
+async function checkAssignment(){
+  const url = new URL(`${window.location.origin}/public/api/index.php`);
+  for (const tr of selectedRows) {
+    const user = tableData.get(Number(tr.dataset.empid));
+
+    url.search = new URLSearchParams({
+      resource: "users",
+      action: "search",
+      search: user.EmpMail,
+    });
+
+    try {
+      const resp = await fetch(url);
+      if (!resp.ok) throw new Error(`HTTP error! status: ${resp.status}`);
+      const data = await resp.json();
+      const assignments = data[0]['assignments'];
+      if (assignments.length>0) return `${user.FName[0]}. ${user.LName}`;
+    } catch (err) {
+      console.error("Error fetching users: ", err);
+    }
+  }
+  return null;
 }
