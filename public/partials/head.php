8e5b857414fd0f01aed4f1cd2246b059046a6799
IvanJunioo <iljunio@up.edu.ph>
2026-02-26 08:07:58 +0800

    fixed select all size and act log table

--- public/partials/head.php
+++ public/partials/head.php
@@ -2,10 +2,10 @@
 <head>
     <meta charset="UTF-8">
     <title>IAMS</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="/../../public/css/general.css">
     <link rel="stylesheet" href="/../../public/css/header.css">
     <link rel="stylesheet" href="/../../public/css/navigation.css">
     <link rel="stylesheet" href="/../../public/css/footer.css">
     <link rel="stylesheet" href="/../../public/css/paging.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
