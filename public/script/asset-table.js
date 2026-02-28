05f0e01c8f91520cb5e4297a4b2261028f39ceaa
IvanJunioo <iljunio@up.edu.ph>
2026-02-28 15:09:56 +0800

    fix api requests endpoints in asset and user-table.js

--- public/script/asset-table.js
+++ public/script/asset-table.js
@@ -108,7 +108,7 @@ document.querySelectorAll(".apply-filter").forEach(btn => {
 
 exportButton.addEventListener("click", () => {
   window.open(
-    `${window.location.origin}/public/api/index.php?resource=export&action=user-assets`,
+    `${window.location.origin}/api/index.php?resource=export&action=user-assets`,
     "_blank"
   );
 })
@@ -131,7 +131,7 @@ async function fetchAssets() {
   [...document.querySelectorAll(".filter-box input[name='status']:checked")].map(cb => cb.value)
 )];
   
-  const url = new URL(`${window.location.origin}/public/api/index.php`);
+  const url = new URL(`${window.location.origin}/api/index.php`);
   url.search = new URLSearchParams({
     resource: "assets",
     action: "search",
