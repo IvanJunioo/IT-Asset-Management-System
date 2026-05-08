69419755571c9dadbb1401a4b202462afcb6e667
Hedelito III Dollison <hmdollison@up.edu.ph>
2026-05-08 17:06:42 +0800

    testing install

--- src/handlers/error.php
+++ src/handlers/error.php
@@ -8,10 +8,10 @@ final class ErrorHandler
   public function handle(
     Throwable $e
   ): never {
-    if ($e instanceof PDOException) {
-      // require_once __DIR__ . '/../views/error.php';
-      exit;
-    }
+    // if ($e instanceof PDOException) {
+    //   require_once __DIR__ . '/../views/error.php';
+    //   exit;
+    // }
 
     if ($e instanceof UserInactiveException) {
       $_SESSION = [];
